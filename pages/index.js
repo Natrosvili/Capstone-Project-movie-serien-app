9ace4254191a5e6fc68aec34adb1dada1e776584
Georgios Natrosvili <georgenatro@gmail.com>
2022-11-30 16:31:04 +0100

    newTry89

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Header from "./Header";
-import Footer from "./Footer"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
       </Head>
       <Header />
 
-      <Footer/>
+      <Footer />
     </>
   );
 }
