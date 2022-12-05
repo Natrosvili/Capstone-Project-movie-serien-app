fd24d428967db277c5e96553215bf69e8ebdaad0
Georgios Natrosvili <georgenatro@gmail.com>
2022-12-05 12:53:17 +0100

    newCommit

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Header from "../components/Header/Header";
+import Header from "../components/Header";
 
 describe("Home", () => {
   it("renders an Anchor", () => {
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import GlobalStyles from "../styles/GlobalStyles";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function MyApp({Component, pageProps}) {
   return (
