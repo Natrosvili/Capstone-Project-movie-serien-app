bbbcd5383b1fdb0569ce9768cf2edf9b93d22cb5
Georgios Natrosvili <georgenatro@gmail.com>
2022-12-05 12:45:51 +0100

    newTry

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,9 @@
 import {render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 
 describe("Home", () => {
-  it("renders a button", () => {
+  it("renders an Anchor", () => {
     render(<Header />);
 
     const text = screen.getByText(/Movies/i);
