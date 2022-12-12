19c0195992fbb79ec2932adfa8a5b93ad3ac4cf5
Georgios Natrosvili <georgenatro@gmail.com>
2022-12-12 14:50:46 +0100

    created Register Page

--- pages/login.js
+++ pages/login.js
@@ -3,70 +3,47 @@ import {useState} from "react";
 import styled from "styled-components";
 
 export default function LogIn() {
-  const [visible, setVisible] = useState("password");
-  const [isTrue, setIsTrue] = useState(false);
-
-  function handleVisibility(event) {
-    if (event.target.value == "on") {
-      setVisible("text");
-      setIsTrue(!isTrue);
-    }
-
-    {
-      isTrue && setVisible("password");
-    }
-  }
+  const [visible, setVisible] = useState(false);
 
   return (
     <>
       <StyledSection>
         <StyledForm method="post" autoComplete="on">
-          <H1>
-            <em>Sign In</em>
-          </H1>
-          <StyledDiv1>
-            <StyledLabel for="account">Your Account:</StyledLabel>
-            <br />
-            <StyledInput1
+          <H1>Sign In</H1>
+          <div style={{margin: "1rem 0"}}>
+            <StyledLabel for="email">Your Account:</StyledLabel>
+            <StyledInput
               type="email"
-              id="account"
-              name="account"
+              id="email"
+              name="email"
               placeholder="Your Email"
               required
             />
-            <br />
-          </StyledDiv1>
-          <StyledDiv2>
+          </div>
+          <div style={{margin: "1rem 0"}}>
             <StyledLabel for="password">Your password:</StyledLabel>
-            <br />
-            <StyledInput2
-              type={visible}
+            <StyledInput
+              type={visible ? "text" : "password"}
               id="password"
               name="password"
               placeholder="Password"
               required
             />
-            <br />
             <input
-              onChange={handleVisibility}
+              onChange={() => setVisible(!visible)}
               type="checkbox"
               id="passwordVisibility"
               name="passwordVisibility"
             />
             <StyledLabel for="passwordVisibility">Show password</StyledLabel>
-          </StyledDiv2>
+          </div>
           <StyledDiv3>
-            <Styledbutton type="submit">
-              <em>Login</em>
-            </Styledbutton>
+            <Styledbutton type="submit">Login</Styledbutton>
           </StyledDiv3>
-          <br />
-          <StyledPar>
-            Don`t have an Account?{" "}
-            <em>
-              <StyledLink href="#">Register.</StyledLink>
-            </em>
-          </StyledPar>
+          <p style={{textAlign: "center"}}>
+            Don`t have an Account?
+            <StyledLink href="/register"> Register.</StyledLink>
+          </p>
         </StyledForm>
       </StyledSection>
     </>
@@ -85,13 +62,18 @@ const StyledForm = styled.form`
   background-color: #9a9595;
   padding: 2rem 1rem 0;
   border-radius: 1.5rem;
-  height: 55vh;
+  height: 50vh;
+
+  @media screen and (max-width: 430px) {
+    height: 50vh;
+  }
 `;
 
 const H1 = styled.h1`
   margin: 0;
   color: #000000;
   margin-bottom: 2rem;
+  font-style: italic;
 `;
 
 const StyledLabel = styled.label`
@@ -99,22 +81,8 @@ const StyledLabel = styled.label`
   cursor: pointer;
 `;
 
-const StyledInput1 = styled.input`
-  width: 38vw;
-  height: 5vh;
-  border: none;
-  /* margin-bottom: 1.5rem; */
-  background-color: #d9d9d9;
-  outline: none;
-  padding-left: 0.8rem;
-
-  @media screen and (max-width: 430px) {
-    width: 42vh;
-  }
-`;
-
-const StyledInput2 = styled.input`
-  width: 38vw;
+const StyledInput = styled.input`
+  width: 50vw;
   height: 5vh;
   border: none;
   background-color: #d9d9d9;
@@ -122,16 +90,8 @@ const StyledInput2 = styled.input`
   padding-left: 0.8rem;
 
   @media screen and (max-width: 430px) {
-    width: 42vh;
+    width: 80vw;
   }
-`;
-
-const StyledDiv1 = styled.div`
-  margin: 1rem 0;
-`;
-
-const StyledDiv2 = styled.div`
-  margin: 1rem 0;
 `;
 
 const StyledDiv3 = styled.div`
@@ -144,17 +104,15 @@ const Styledbutton = styled.button`
   color: #ffffff;
   border: none;
   border-radius: 0.8rem;
-  width: 60vw;
+  width: 40vw;
   height: 5vh;
   cursor: pointer;
   margin-top: 0.5rem;
-`;
-
-const StyledPar = styled.p`
-  text-align: center;
+  font-style: italic;
 `;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
   color: #d11818;
+  font-style: italic;
 `;
